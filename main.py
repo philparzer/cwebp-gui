299cb9a94abc12f1dde729654078cc47dce6a77e
philparzer <phil0898.pp@gmail.com>
2022-11-23 16:57:49 +0100

    add file detection

--- main.py
+++ main.py
@@ -4,12 +4,8 @@ from os import listdir
 from os.path import isfile, join
 import PySimpleGUI as sg
 
-grant_permission()
-#TODO: move below on submit
-# pass input_image(.jpeg,.pnp .....) path ,
-# output_image(give path where to save and image file name with .webp file type extension)
-# print(cwebp(input_image="test.png", output_image="test.webp",
-#             option="-q 80", logging="-v"))
+grant_permission() #solves no webptools permission bug
+
 
 sg.theme('systemdefault')
 
@@ -21,9 +17,19 @@ window = sg.Window('Compile to webp', layout)
 
 event, values = window.read()
 window.close()#TODO: don't close on submit
+
 print(f'You clicked on: {event}')
 print(f'You chose the folder: {values[0]}')
+
 onlyfiles = [f for f in listdir(values[0]) if isfile(join(values[0], f))]
 for f in onlyfiles:
-  print(f)
-  #TODO: get images
+  if f.lower().endswith(('.png', '.jpg', '.jpeg')): #TODO: look into valid image file types for webptools
+    print("found image: " + f)
+    #TODO: add to array
+
+
+#TODO: use array from above and pass each item
+# pass input_image(.jpeg,.pnp .....) path ,
+# output_image(give path where to save and image file name with .webp file type extension) #TODO: get relative path to images from app?
+# print(cwebp(input_image="test.png", output_image="test.webp",
+#             option="-q 80", logging="-v"))
