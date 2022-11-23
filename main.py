02d7e16813b7ff096ad63a9ecc14f3d916d02cf8
philparzer <phil0898.pp@gmail.com>
2022-11-23 20:28:44 +0100

    add comments

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from os import listdir
 from os.path import isfile, join, relpath, curdir
 import PySimpleGUI as sg
 
-grant_permission() #solves no webptools permission bug
+#if os restricts webptools permission
+grant_permission() 
 
-
+#GUI
 sg.theme('systemdefault')
 
 layout = [[sg.Text('Pick a folder to batch-convert to webp', font="Helvetica 14")], #TODO: style
@@ -18,13 +19,14 @@ window = sg.Window('Compile to webp', layout)
 event, values = window.read()
 window.close()#TODO: don't close on submit
 
+#cwebp
 onlyfiles = [f for f in listdir(values[0]) if isfile(join(values[0], f))]
 for f in onlyfiles:
   if f.lower().endswith(('.png', '.jpg', '.jpeg')): #TODO: look into valid image file types for webptools
     path = relpath(join(values[0], f), start = curdir) 
-    
+    #TODO: use os.splitext
     #TODO: create option input field to set quality
     #TODO: strip file extension and replace with webp
     cwebp(input_image=path, output_image=f'{path}.webp', option="-q 80", logging="-v") 
-    
+
   #TODO: raise something if no images found
