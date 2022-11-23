be60918ba23f1c7a3f597e440e743ef3cb2f298d
philparzer <phil0898.pp@gmail.com>
2022-11-23 21:40:32 +0100

    add error messages

--- main.py
+++ main.py
@@ -10,23 +10,54 @@ grant_permission()
 #GUI
 sg.theme('systemdefault')
 
-layout = [[sg.Text('Pick a folder to batch-convert to webp', font="Helvetica 14")], #TODO: style
-          [sg.Text('Folder', size=(8, 1), font="Helvetica 14"), sg.Input(), sg.FolderBrowse()],
-          [sg.Submit()]]
+layout = [
+          [sg.Text('Image Folder', font="Helvetica 14"), sg.Input(key="-PATH-"), sg.FolderBrowse(font="Helvetica 14")],
+          [sg.Text('Compression', font="Helvetica 14"), sg.Input(key="-COMPRESSION_RATE-", font="Helvetica 14", size=(4,1), tooltip="n most recent tweets to be scraped", default_text="80"),sg.Text('%', font="Helvetica 14")],
+          [sg.Button('Start', font="Helvetica 14", pad=((5,5),(20,0)), button_color="green")],
+          [sg.Text('Status', key='-STATUS-', pad=((5,0),(5,0)), font="Helvetica 10")]
+        ]
+          
 
 window = sg.Window('Compile to webp', layout)
 
-event, values = window.read()
-window.close()#TODO: don't close on submit
+while True:
 
-#cwebp
-onlyfiles = [f for f in listdir(values[0]) if isfile(join(values[0], f))]
-for f in onlyfiles:
-  if f.lower().endswith(('.png', '.jpg', '.jpeg')): #TODO: look into valid image file types for webptools
-    path = relpath(join(values[0], f), start = curdir) 
-    #TODO: use os.splitext
-    #TODO: create option input field to set quality
-    #TODO: strip file extension and replace with webp
-    cwebp(input_image=path, output_image=f'{path}.webp', option="-q 80", logging="-v") 
+  event, values = window.read()
 
-  #TODO: raise something if no images found
+  if event == 'Start':
+    try:
+      compression_rate = int(values['-COMPRESSION_RATE-'])
+      if (compression_rate < 0 or compression_rate > 100): 
+        window['-STATUS-'].update('Enter an integer between 0-100', text_color='red')
+        continue
+
+    except Exception as e:
+      window['-STATUS-'].update('Enter a valid integer', text_color='red')
+      continue
+
+    if values["-PATH-"] == '':
+        window['-STATUS-'].update('Pick a folder', text_color='red')
+        continue
+
+    #cwebp
+    onlyfiles = [f for f in listdir(values["-PATH-"]) if isfile(join(values["-PATH-"], f))]
+    converted_images = 0
+    compression_rate
+    for f in onlyfiles:
+      if f.lower().endswith(('.png', '.jpg', '.jpeg')): #TODO: look into valid image file types for webptools
+        path = relpath(join(values["-PATH-"], f), start = curdir) 
+        #TODO: use os.splitext
+        #TODO: create option input field to set quality
+        #TODO: strip file extension and replace with webp
+        cwebp(input_image=path, output_image=f'{path}.webp', option=f"-q {values['-COMPRESSION_RATE-']}", logging="-v") 
+        converted_images += 1
+
+    else:
+      if(converted_images > 0): window["-STATUS-"].update(f"Converted {converted_images} images", text_color='green')
+      else: window["-STATUS-"].update("Found no valid images to convert", text_color='yellow')
+  if event == sg.WIN_CLOSED or event == 'Exit':
+    break
+
+
+window.close()
+quit() #FIXME: when compiled
