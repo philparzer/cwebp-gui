ef27bc14fe6ef20c70872bd31e04821f7119a4d1
philparzer <phil0898.pp@gmail.com>
2022-11-23 16:47:56 +0100

    add os

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from webptools import grant_permission
 from webptools import cwebp
+from os import listdir
+from os.path import isfile, join
+import PySimpleGUI as sg
 
 grant_permission()
+#TODO: move below on submit
 # pass input_image(.jpeg,.pnp .....) path ,
 # output_image(give path where to save and image file name with .webp file type extension)
 # print(cwebp(input_image="test.png", output_image="test.webp",
 #             option="-q 80", logging="-v"))
-
-import PySimpleGUI as sg
 
 sg.theme('systemdefault')
 
@@ -18,6 +20,10 @@ layout = [[sg.Text('Pick a folder to batch-convert to webp', font="Helvetica 14"
 window = sg.Window('Compile to webp', layout)
 
 event, values = window.read()
-window.close()
+window.close()#TODO: don't close on submit
 print(f'You clicked on: {event}')
 print(f'You chose the folder: {values[0]}')
+onlyfiles = [f for f in listdir(values[0]) if isfile(join(values[0], f))]
+for f in onlyfiles:
+  print(f)
+  #TODO: get images
