d300b0cab567682c47e7d4ca426f584749988434
philparzer <phil0898.pp@gmail.com>
2022-11-23 19:54:07 +0100

    add compile

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from webptools import grant_permission
 from webptools import cwebp
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, relpath, curdir
 import PySimpleGUI as sg
 
 grant_permission() #solves no webptools permission bug
@@ -9,7 +9,7 @@ grant_permission() #solves no webptools permission bug
 
 sg.theme('systemdefault')
 
-layout = [[sg.Text('Pick a folder to batch-convert to webp', font="Helvetica 14")],
+layout = [[sg.Text('Pick a folder to batch-convert to webp', font="Helvetica 14")], #TODO: style
           [sg.Text('Folder', size=(8, 1), font="Helvetica 14"), sg.Input(), sg.FolderBrowse()],
           [sg.Submit()]]
 
@@ -18,18 +18,13 @@ window = sg.Window('Compile to webp', layout)
 event, values = window.read()
 window.close()#TODO: don't close on submit
 
-print(f'You clicked on: {event}')
-print(f'You chose the folder: {values[0]}')
-
 onlyfiles = [f for f in listdir(values[0]) if isfile(join(values[0], f))]
 for f in onlyfiles:
   if f.lower().endswith(('.png', '.jpg', '.jpeg')): #TODO: look into valid image file types for webptools
-    print("found image: " + f)
-    #TODO: add to array
-
-
-#TODO: use array from above and pass each item
-# pass input_image(.jpeg,.pnp .....) path ,
-# output_image(give path where to save and image file name with .webp file type extension) #TODO: get relative path to images from app?
-# print(cwebp(input_image="test.png", output_image="test.webp",
-#             option="-q 80", logging="-v"))
+    path = relpath(join(values[0], f), start = curdir) 
+    
+    #TODO: create option input field to set quality
+    #TODO: strip file extension and replace with webp
+    cwebp(input_image=path, output_image=f'{path}.webp', option="-q 80", logging="-v") 
+    
+  #TODO: raise something if no images found
